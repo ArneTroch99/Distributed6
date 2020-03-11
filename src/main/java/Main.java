322e664d98da643c73c3bcffd829c16fe3c1dd7c
ArneTroch <44033131+ArneTroch@users.noreply.github.com>
2020-03-11 15:57:24 +0100

    niedfkqsd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Fuck you Siemendfgfgddfg");
 
         System.out.println("Giles heeft een kleine, Siemen een gigantische, Arne nen wolkenkrabber");
+
+        GreetClient client = new GreetClient();
+        client.startConnection("localhost", 8888);
+        client.sendMessage("Penis");
+        client.stopConnection();
+
     }
 }
