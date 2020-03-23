4253b71a0b9bf1de1c57b07745004f0926ea0670
ArneTroch99 <arnetroch@gmail.com>
2020-03-23 14:28:24 +0000

    blabla

---
+++ src/main/java/MainServer.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class MainServer {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Fck you Siemendfgfgddfg");
+
+        System.out.println("Giles heeft een kleine, Siemen een gigantische, Arne nen wolkenkrabber");
+
+        GreetServer server = new GreetServer();
+	server.start(8888);
+
+    }
+}
