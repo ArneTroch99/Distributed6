9451954343be2079a44de2c901afb664f5aff2a2
siemenHerremans <siemen_herremans@hotmail.com>
2020-03-23 14:37:24 +0000

    Siemen is kei hot

---
+++ src/main/java/MainClient.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class MainClient{
+
+    public static void main(String[] args) throws IOException {
+       
+        GreetClient client = new GreetClient();
+        client.startConnection("10.0.13.14", 8888);
+        client.sendMessage("hello server");
+        client.stopConnection();
+
+    }
+}
